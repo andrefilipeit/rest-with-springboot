66a299fb2885a2638a1542f6f28c47fad79bc166
Andre Filipe <andre.filipe@partners.mv.com.br>
2022-05-12 13:34:38 -0300

    deleting all trashes

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -1,13 +1,12 @@
-package br.com.andrefilipeos;
+package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Startup {
+
 	public static void main(String[] args) {
-		
-		//main Class with sinalization to spring that this class is the our inicializer application
 		SpringApplication.run(Startup.class, args);
 	}
 }
